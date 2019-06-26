df35cd0d1af336cd74a4024e5b84c4a39d4cf94f
Dmitry Yan <rocketman27@inbox.ru>
2019-06-26 13:06:23 +0400

    Почему-бы не выгрузить это сюда?

---
+++ engine-messages-en/init.php
@@ -0,0 +1,15 @@
+<?php
+
+  $_Lang = array(
+    'cct_err' => 'Connect error',
+    'con_establ' => 'Connection Established',
+    'err' => 'Error',
+    'y' => 'Yes',
+    'n' => 'No',
+    'cfg' => 'Configuration',
+    'main' => 'Main',
+    'del' => 'Delete',
+    'authrz' => 'Authorize',
+    'install_pkg' => 'Install Package',
+    'pages' => 'Pages'
+  );
